7c4378fb3ae17f7ecccb62231d34efe793a2198e
Miguel CR <camposmiguel@gmail.com>
2018-10-04 13:30:04 +0200

    explico el toggle

--- UD02_jQuery/02_DOM/js/custom.js
+++ UD02_jQuery/02_DOM/js/custom.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
     $('.btn-fav').click(function (e) { 
         e.preventDefault();
-        $(this).toggleClass('btn-subtle');
 
+        // Si en el elemento seleccionado, existe la clase css class="btn-subtle"
+        // el método toggleClass, elimina dicha clase
+        // en caso de que no exista, la añadiría
+        $(this).toggleClass('btn-subtle');
         $(this).closest('.card').toggleClass('card-primary card-inverse')
     });
 });
